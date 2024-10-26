e25bc472f127df6dcae2989cdee2eded15aa15cf
Abhishek pandey <pandeybless@gmail.com>
2024-10-26 20:58:22 +0530

    add a function in calculator

--- calculator.py
+++ calculator.py
@@ -2,16 +2,25 @@ a=int(input("enter first number: "))
 b=int(input("enter second number: "))
 sank=input("enter what you want to do: ")
 
+# here created conditional statement according to input given by user what user want to 
+def text():
+    print("Your Answer is here:")
+
 
 if(sank=="+") :
+    text()
     print(a+b)
 
 elif(sank=="-"):
+    text()
     print(a-b)
 elif(sank=="x"):
+    text()
     print(a*b)
 elif(sank=="/"):
+    text()
     print(a/b)
 else :
+     text()
      print(a%b)
     
